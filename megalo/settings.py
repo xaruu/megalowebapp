8680fb1ac99284fd56317439f93bb7ba7b4c059a
alex <alexdwvar@gmail.com>
2019-08-01 14:28:03 -0500

    asd

--- megalo/settings.py
+++ megalo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ijj$sq#c=8siap0l12v+94swj-i02w=vbu2+phftv@3)d(49vl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
