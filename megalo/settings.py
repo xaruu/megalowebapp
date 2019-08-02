55fd7d4105428206131c1691e876aa336f211fe5
alex <alexdwvar@gmail.com>
2019-08-01 19:32:09 -0500

    asdSd

--- megalo/settings.py
+++ megalo/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'web'
 ]
 
+# import whitenoise
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,5 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
+# STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
